9d70f17ca33c063bb3ba3fe84ccf41c39f8512df
fabianormatias <fabiano@waiso.com.br>
2015-07-08 11:03:54 -0300

    ajuste metodo sha512

--- criptografia/src/main/java/br/com/waiso/encrypt/sha512/app/EncriptaDecriptaSHA512.java
+++ criptografia/src/main/java/br/com/waiso/encrypt/sha512/app/EncriptaDecriptaSHA512.java
@@ -5,6 +5,9 @@ import org.apache.commons.codec.digest.DigestUtils;
 public class EncriptaDecriptaSHA512 {
 	
 	private String salt = "uuid-7981383252074976350";
+	private StringBuilder sb;
+	private byte[] hash;
+	private String hexadecimal;
 	
 	private static EncriptaDecriptaSHA512 instance = new EncriptaDecriptaSHA512();
 
@@ -16,8 +19,9 @@ public class EncriptaDecriptaSHA512 {
 	}
 	
 	public String encriptarSHA512(String textoPuro) {
-		byte[] hash = DigestUtils.sha512((textoPuro + salt).getBytes());
-		String hexadecimal = DigestUtils.sha512Hex(hash);
+		sb = new StringBuilder(textoPuro);
+		hash = DigestUtils.sha512((sb.append(salt).toString()).getBytes());
+		hexadecimal = DigestUtils.sha512Hex(hash);
 		return hexadecimal;
 	}
 	
